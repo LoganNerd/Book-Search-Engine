f5d1616b2350841e0ecafbfd63334c06c4264e37
Your Name <you@example.com>
2023-03-22 14:39:40 -0600

    Finishing up typeDef and Resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String, email: String, password: String): Auth
     login(email: String, password: String): Auth
-    savebook(bookData: bookInput): User
+    saveBook(bookData: bookInput): User
     removeBook(bookId: ID): User
   }
 `;
