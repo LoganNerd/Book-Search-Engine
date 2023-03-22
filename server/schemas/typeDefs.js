98f3466757adbce9da6af833cb1918ce6c783880
Your Name <you@example.com>
2023-03-22 14:29:26 -0600

    Adding Bookinput and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,12 +16,28 @@ const typeDefs = gql`
     link: String
     title: String
   }
+  input bookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+
   type Query {
-    
+    me: User
   }
 
   type Mutation {
-    
+    addUser(username: String, email: String, password: String): Auth
+    login(email: String, password: String): Auth
+    savebook(bookData: bookInput): User
+    removeBook(bookId: ID): User
   }
 `;
 
